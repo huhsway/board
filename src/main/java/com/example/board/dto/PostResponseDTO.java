db4c6ee7f2ec79007ed3559b8c9a756f621f8680
huhsway <seunghoe9542@naver.com>
2024-09-19 01:00:31 +0900

    빌더 수정

--- src/main/java/com/example/board/dto/PostResponseDTO.java
+++ src/main/java/com/example/board/dto/PostResponseDTO.java
@@ -12,14 +12,13 @@ import java.util.stream.Collectors;
 @Setter
 @Builder
 public class PostResponseDTO {
-
     private Long id;
     private Long userId;
     private String title;
     private String body;
     private List<CommentResponseDTO> comments;
 
-    private PostResponseDTO postResponseDTO(Post post) {
+    public static PostResponseDTO from(Post post) {
         return PostResponseDTO.builder()
                 .id(post.getId())
                 .userId(post.getUserId())
@@ -30,5 +29,4 @@ public class PostResponseDTO {
                         .toList())
                 .build();
     }
-
 }
