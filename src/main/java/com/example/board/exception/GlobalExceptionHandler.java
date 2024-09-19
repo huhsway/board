98aa5e980ab4b014711fa1e56ca4c10b62f652be
huhsway <seunghoe9542@naver.com>
2024-09-19 20:01:02 +0900

    로거 추가

--- src/main/java/com/example/board/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/board/exception/GlobalExceptionHandler.java
@@ -1,30 +1,36 @@
 package com.example.board.exception;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+@Slf4j
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(PostNotFoundException.class)
     public ResponseEntity<String> handlePostNotFoundException(PostNotFoundException ex) {
+        log.error("Post not found: {}", ex.getMessage());
         return new ResponseEntity<>(ex.getMessage(), HttpStatus.NOT_FOUND);
     }
 
     @ExceptionHandler(CsvProcessingException.class)
     public ResponseEntity<String> handleCsvProcessingException(CsvProcessingException ex) {
+        log.error("CSV processing error: {}", ex.getMessage());
         return new ResponseEntity<>(ex.getMessage(), HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(ExternalApiException.class)
     public ResponseEntity<String> handleExternalApiException(ExternalApiException ex) {
+        log.error("External API error: {}", ex.getMessage());
         return new ResponseEntity<>(ex.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<String> handleGeneralException(Exception ex) {
-        return new ResponseEntity<>("An unexpected error occured: " + ex.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+        log.error("An unexpected error occurred: {}", ex.getMessage(), ex);
+        return new ResponseEntity<>("An unexpected error occurred: " + ex.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
